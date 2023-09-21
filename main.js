adefe7e1637dacb3bb3427bd82ffdd89975311d0
Bogdan Kudryashov <bogdan.kudryashov.06@mail.ru>
2023-09-21 15:24:41 +0300

    pre-release

---
+++ main.js
@@ -0,0 +1,8 @@
+function OpenMenu() {
+    document.getElementById("sub_list").classList.toggle('active');
+    document.getElementById("menu").classList.toggle('active_menu');
+}
+function OpenMenu2() {
+    document.getElementById("sub_list2").classList.toggle('active');
+    document.getElementById("menu2").classList.toggle('active_menu');
+}
